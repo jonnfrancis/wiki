69b944ab26cf5cae9ba496dbbf8be922e608d039
John Francis <johnmosima7@gmail.com>
2021-07-06 16:38:37 +0300

    update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("wiki/<str:entry>",views.entry, name="entry"),
     path("random", views.random, name="random"),
     path("wiki/<str:entry>/edit",views.edit, name="edit"),
-    path("search", views.search, name="search"),
     path("newEntry", views.newEntry, name="newEntry")
 ]
